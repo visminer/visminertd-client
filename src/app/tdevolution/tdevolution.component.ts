3401ba1c60169ebfe9ec75b8334bc08e9731266b
DavidPinho <davidpinho08@gmail.com>
2018-01-07 02:22:34 +0000

    [tdevolution] Add highcharts stacked column

--- src/app/tdevolution/tdevolution.component.ts
+++ src/app/tdevolution/tdevolution.component.ts
@@ -3,6 +3,7 @@ import { Repository } from '../shared/models/Repository';
 import { Reference } from '../shared/models/Reference';
 import { VisminerService } from '../shared/services/visminer.service';
 import { NouisliderComponent } from 'ng2-nouislider/src/nouislider';
+import { ChartComponent } from 'angular2-highcharts';
 
 @Component({
   selector: 'app-tdevolution',
@@ -14,6 +15,7 @@ export class TDEvolutionComponent implements OnInit {
   references: Reference[]; 
   sliderRange: number[];  
   sliderConfig: any;
+  chartConfig: Object;
 
   constructor(private visminerServ: VisminerService) { }
 
@@ -21,6 +23,7 @@ export class TDEvolutionComponent implements OnInit {
     this.repository = this.visminerServ.repository;
     this.references = this.visminerServ.references;
     this.loadSlider();
+    this.loadChart();
   }
 
   loadSlider() {
@@ -47,6 +50,86 @@ export class TDEvolutionComponent implements OnInit {
         }
       }
     };
+  }
+
+  loadChart() {
+    var references = this.references;
+    var seriesArray = [];
+		seriesArray.push({
+			color: '#dd3939',
+			name: 'Defect Debt',
+			data: [29.9, 71.5, 106.4, 129.2],
+		});
+		seriesArray.push({
+			color: '#f39c12',
+			name: 'Test Debt',
+			data: [29.9, 71.5, 106.4, 129.2],
+		});
+		seriesArray.push({
+			color: '#8a6d3b',
+			name: 'Requirement Debt',
+			data: [29.9, 71.5, 106.4, 129.2],
+		});
+    seriesArray.push({
+      color: '#1B93A7',
+      name: 'Code Debt',
+      data: [29.9, 71.5, 106.4, 129.2],
+    });
+    seriesArray.push({
+      color: '#91A28B',
+      name: 'Design Debt',
+      data: [29.9, 71.5, 106.4, 129.2],
+    });
+    this.chartConfig = {
+      title : { text : 'Technical Debt X Versions' },
+      xAxis: { categories: references.map(ref => ref.name) },
+      yAxis: {
+				min: 0,
+				allowDecimals: false,
+				title: { text: 'Total of classes having Technical Debt' },
+				stackLabels: {
+					enabled: true,
+					style: {
+						fontWeight: 'bold',
+						color: 'gray'
+					}
+        }
+      },  
+      chart: {
+        type: 'column',
+        width: 1014 // TODO: Make it 100% of parent div instead of setting statically
+      },
+      legend: {
+        align: 'center',
+        verticalAlign: 'top',
+        y: 20,
+        floating: true,
+        backgroundColor: 'white',
+        borderColor: '#CCC',
+        borderWidth: 1,
+        shadow: false
+      },
+      tooltip: {
+        formatter: function () {
+          return '<b>' + this.x + '</b><br/>' +
+            this.series.name + ': ' + this.y + '<br/>' +
+            'Total: ' + this.point.stackTotal;
+        }
+      },
+      plotOptions: {
+        column: {
+          stacking: 'normal',
+          dataLabels: {
+            enabled: true,
+            color: 'white',
+            style: {
+              textShadow: '0 0 3px black, 0 0 3px black'
+            }
+          }
+        }
+      },
+      series: seriesArray
+  };
   }
 
 }
--- src/app/tdevolution/tdevolution.module.ts
+++ src/app/tdevolution/tdevolution.module.ts
@@ -3,12 +3,26 @@ import { CommonModule } from '@angular/common';
 import { TDEvolutionComponent } from './tdevolution.component';
 import { TDEvolutionBoxesComponent } from './boxes/boxes.component';
 import { NouisliderComponent } from 'ng2-nouislider/src/nouislider';
+import { ChartModule } from 'angular2-highcharts';
+import { HighchartsStatic } from 'angular2-highcharts/dist/HighchartsService';
+
+declare var require: any;
+export function highchartsFactory() {
+  return require('highcharts');
+}
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    ChartModule
   ],
   declarations: [TDEvolutionComponent, TDEvolutionBoxesComponent, NouisliderComponent],
+  providers: [
+    {
+      provide: HighchartsStatic,
+      useFactory: highchartsFactory
+    },
+  ],
   exports: [TDEvolutionComponent]
 })
 export class TDEvolutionModule { }
