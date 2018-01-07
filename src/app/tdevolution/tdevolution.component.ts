2ea8c6a53167a91f744515f23cda100591c01496
DavidPinho <davidpinho08@gmail.com>
2018-01-07 00:31:39 +0000

    [tdevolution] Create slider

--- src/app/tdevolution/tdevolution.component.ts
+++ src/app/tdevolution/tdevolution.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Repository } from '../shared/models/Repository';
+import { Reference } from '../shared/models/Reference';
 import { VisminerService } from '../shared/services/visminer.service';
+import { NouisliderComponent } from 'ng2-nouislider/src/nouislider';
 
 @Component({
   selector: 'app-tdevolution',
@@ -8,13 +10,43 @@ import { VisminerService } from '../shared/services/visminer.service';
   styleUrls: ['./tdevolution.component.css']
 })
 export class TDEvolutionComponent implements OnInit {
-
-  repository: Repository;
+  repository: Repository; 
+  references: Reference[]; 
+  sliderRange: number[];  
+  sliderConfig: any;
 
   constructor(private visminerServ: VisminerService) { }
 
   ngOnInit() {
     this.repository = this.visminerServ.repository;
+    this.references = this.visminerServ.references;
+    this.loadSlider();
+  }
+
+  loadSlider() {
+    var references = this.references;
+    var max = references.length - 1;
+    this.sliderRange = [0, max];
+    this.sliderConfig = {
+      behaviour: 'drag',
+      connect: true,
+      start: [0, max],
+      keyboard: true,
+      step: 1,
+      range: {
+        min: 0,
+        max: max
+      },
+      pips: {
+        mode: 'steps',
+        density: 0,
+        format: {
+          to: function(value){
+          return references[value].name;
+          }
+        }
+      }
+    };
   }
 
 }
--- src/app/tdevolution/tdevolution.module.ts
+++ src/app/tdevolution/tdevolution.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TDEvolutionComponent } from './tdevolution.component';
 import { TDEvolutionBoxesComponent } from './boxes/boxes.component';
+import { NouisliderComponent } from 'ng2-nouislider/src/nouislider';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [TDEvolutionComponent, TDEvolutionBoxesComponent],
+  declarations: [TDEvolutionComponent, TDEvolutionBoxesComponent, NouisliderComponent],
   exports: [TDEvolutionComponent]
 })
 export class TDEvolutionModule { }
