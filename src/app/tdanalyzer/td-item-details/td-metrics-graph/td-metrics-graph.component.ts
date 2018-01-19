280a44c558cc09477b479e25c733e8a24e2658d5
Felipe Gustavo de Souza Gomes <felipegustavo1000@gmail.com>
2018-01-19 11:14:39 -0300

    [tdanalyzer] td graph almost done, just need to solve small bugs.

--- src/app/tdanalyzer/td-item-details/td-metrics-graph/td-metrics-graph.component.ts
+++ src/app/tdanalyzer/td-item-details/td-metrics-graph/td-metrics-graph.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { TDMetricsGraphService } from './td-metrics-graph.service';
 import { TDItem } from './../../../shared/models/TDItem';
 import { CodeAnalysis } from '../../../shared/models/CodeAnalysis';
+import { version } from 'moment';
 
 @Component({
   selector: 'app-td-metrics-graph',
@@ -12,13 +13,14 @@ import { CodeAnalysis } from '../../../shared/models/CodeAnalysis';
 export class TDMetricsGraphComponent implements OnInit {
 
   @Input() tdItem: TDItem;
-
   analysisHistory: CodeAnalysis[] = [];
+
   chart: any;
+  versions: string[];
+  metricValues: number[];
 
   clazz: string;
   metric: string;
-  versions: string[];
 
   allClasses: Set<string>;
   allMetrics: string[] = [];
@@ -41,12 +43,76 @@ export class TDMetricsGraphComponent implements OnInit {
         this.allMetrics = analysisHistory[0].classes[0].metrics.map(elem => elem.name);
         this.metric = this.allMetrics[0];
       }
+
+      this.updateChart();
     });
   }
 
   updateChart() {
-    console.log(this.clazz);
-    console.log(this.metric);
+    this.versions = [];
+    this.metricValues = [];
+
+    this.analysisHistory.forEach(analysis => {
+      for (let i = 0; i < analysis.classes.length; i++) {
+        const tmpCls = analysis.classes[i];
+        if (tmpCls.name === this.clazz) {
+          this.versions.push(analysis.reference);
+          for (let y = 0; y < tmpCls.metrics.length; y++) {
+            if (tmpCls.metrics[i].name === this.metric) {
+              this.metricValues.push(tmpCls.metrics[i].value);
+              break;
+            }
+          }
+          break;
+        }
+      }
+    });
+
+    this.drawChart();
+    console.log(this.versions);
+    console.log(this.metricValues);
+  }
+
+  private drawChart() {
+    this.chart = {
+      title: {
+        text: 'Metrics Evolution'
+      },
+      subtitle: {
+        text: this.metric
+      },
+      xAxis: {
+        title: {
+          text: 'Versions'
+        },
+        categories: this.versions
+      },
+      yAxis: {
+        min: 0,
+        title: {
+          text: 'Metrics Values'
+        },
+        stackLabels: {
+          enabled: true,
+          style: {
+            fontWeight: 'bold'
+          }
+        }
+      },
+      credits: {
+        enabled: false
+      },
+      options: {
+        chart: {
+          type: 'line',
+          zoomType: 'x'
+        }
+      },
+      series: {
+        type: 'line',
+        data: this.metricValues
+      }
+    };
   }
 
 }
