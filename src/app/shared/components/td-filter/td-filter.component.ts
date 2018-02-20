855d97b414a021d55ad08de38e03731a5f6e6cb8
Felipe Gustavo de Souza Gomes <felipegustavo1000@gmail.com>
2018-02-19 22:23:47 -0300

    Added comment analysis indicators to the TD filter.

--- src/app/shared/components/td-filter/td-filter.component.ts
+++ src/app/shared/components/td-filter/td-filter.component.ts
@@ -20,6 +20,9 @@ export class TDFilterComponent implements OnInit {
 
   ngOnInit() {
     this.indicatorsList = [
+      // Unknown Debt
+      { 'id': 'COMMENT_ANALYSIS_UNKNOWN_DEBT', itemName: 'Comment Analysis', category: 'Unknown Debt'}
+
       // Code Debt
       { 'id': 'GOD_CLASS', itemName: 'God Class', category: 'Code Debt' },
       { 'id': 'COMPLEX_METHOD', itemName: 'Complex Method', category: 'Code Debt' },
@@ -31,6 +34,7 @@ export class TDFilterComponent implements OnInit {
       { 'id': 'AUTOMATIC_STATIC_ANALYSIS_ISSUES', itemName: 'Automatic Static Analysis Issues', category: 'Code Debt' },
       { 'id': 'MULTITHREAD_CORRECTNESS', itemName: 'Multithread Correctness', category: 'Code Debt' },
       { 'id': 'SLOW_ALGORITHM', itemName: 'Slow Algorithm', category: 'Code Debt' },
+      { 'id': 'COMMENT_ANALYSIS_CODE_DEBT', itemName: 'Comment Analysis', category: 'Code Debt'},
 
       // Design Debt
       { 'id': 'GOD_CLASS', itemName: 'God Class', category: 'Design Debt' },
@@ -39,7 +43,29 @@ export class TDFilterComponent implements OnInit {
       { 'id': 'BRAIN_METHOD', itemName: 'Brain Method', category: 'Design Debt' },
       { 'id': 'DATA_CLASS', itemName: 'Data Class', category: 'Design Debt' },
       { 'id': 'DUPLICATED_CODE', itemName: 'Duplicated Code', category: 'Design Debt' },
-      { 'id': 'AUTOMATIC_STATIC_ANALYSIS_ISSUES', itemName: 'Automatic Static Analysis Issues', category: 'Design Debt' }
+      { 'id': 'AUTOMATIC_STATIC_ANALYSIS_ISSUES', itemName: 'Automatic Static Analysis Issues', category: 'Design Debt' },
+      { 'id': 'COMMENT_ANALYSIS_DESIGN_DEBT', itemName: 'Comment Analysis', category: 'Design Debt'},
+
+      // Architecture Debt
+      { 'id': 'COMMENT_ANALYSIS_ARCHITECTURE_DEBT', itemName: 'Comment Analysis', category: 'Architecture Debt'},
+
+      // Build Debt
+      { 'id': 'COMMENT_ANALYSIS_BUILD_DEBT', itemName: 'Comment Analysis', category: 'Build Debt'},
+
+      // Defect Debt
+      { 'id': 'COMMENT_ANALYSIS_DEFECT_DEBT', itemName: 'Comment Analysis', category: 'Defect Debt'},
+
+      // Documentation Debt
+      { 'id': 'COMMENT_ANALYSIS_DOCUMENTATION_DEBT', itemName: 'Comment Analysis', category: 'Documentation Debt'},
+
+      // People Debt
+      { 'id': 'COMMENT_ANALYSIS_PEOPLE_DEBT', itemName: 'Comment Analysis', category: 'People Debt'},
+
+      // Requirement Debt
+      { 'id': 'COMMENT_ANALYSIS_REQUIREMENT_DEBT', itemName: 'Comment Analysis', category: 'Requirement Debt'},
+
+      // Test Debt
+      { 'id': 'COMMENT_ANALYSIS_TEST_DEBT', itemName: 'Comment Analysis', category: 'Test Debt'}
     ];
 
     this.indicatorsSettings = {
